c02abc82078e601a7775bf04c6dff3bd9ddba248
ipigbeitsenes <66330932+ipigbeitsenes@users.noreply.github.com>
2021-02-11 13:56:44 -0600

    Update CustomBtn.js

--- components/CustomBtn.js
+++ components/CustomBtn.js
@@ -1,29 +1,31 @@
 import React from "react";
+import styled from "styled-components/native";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const CustomBtn = React.forwardRef((props, ref) => {
+  const Container = styled.View`
+    height: 98px;
+    width: 100px;
+    border-color: #333333;
+    border-width: 1px;
+    background-color: ${props.label ? "#8e2525" : "#555"};
+    align-items: center;
+    justify-content: center;
+    border-radius: 3px;
+  `;
+
   return (
     <TouchableOpacity onPress={props.myfun}>
-      <View style={styles.num} ref={ref} className="CustomBtn">
+      <Container>
         <Text style={styles.txtfont}>{props.label}</Text>
-      </View>
+      </Container>
     </TouchableOpacity>
   );
 });
 
-export default CustomBtn;
-
 const styles = StyleSheet.create({
-  num: {
-    height: 98,
-    width: 100,
-    borderColor: "#333333",
-    borderWidth: 1,
-    backgroundColor: "#8e2525",
-    alignItems: "center",
-    justifyContent: "center",
-  },
   txtfont: {
     fontSize: 60,
   },
 });
+export default CustomBtn;
